65f1cd4a9d47c97cad9cb8dc48cd7ab94e5dd58b
Sparrow <admin@sparrowhawks-Mac-mini.local>
2018-09-10 21:26:12 -0500

    Project boiler plate. Empty. Nothingness.

---
+++ PierreThePenguinDos/GameScene.swift
@@ -0,0 +1,15 @@
+//
+//  GameScene.swift
+//  PierreThePenguinDos
+//
+//  Created by Sparrow on 9/10/18.
+//  Copyright © 2018 ParanoidWacko. All rights reserved.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    override func didMove(to view: SKView) {
+        
+    }
+}
