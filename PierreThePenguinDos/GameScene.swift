649082be411acb61e89e90d815dd9aae8287c6c3
Sparrow <admin@sparrowhawks-Mac-mini.local>
2018-10-06 17:22:54 -0500

    Silly little blue square floating, expanding and rotating in space

--- PierreThePenguinDos/GameScene.swift
+++ PierreThePenguinDos/GameScene.swift
@@ -10,6 +10,21 @@ import SpriteKit
 
 class GameScene: SKScene {
     override func didMove(to view: SKView) {
+        self.anchorPoint = .zero
         
+        let mySprite = SKSpriteNode(color: .blue, size: CGSize(width: 50, height: 50))
+        mySprite.position = CGPoint(x: 150, y: 150)
+//        mySprite.anchorPoint = CGPoint(x: 0, y: 0)
+        self.addChild(mySprite)
+        
+//        let demoAction = SKAction.move(to: CGPoint(x: 300, y: 150), duration: 3)
+//        let demoAction = SKAction.scale(to: 4, duration: 5)
+        let demoAction1 = SKAction.scale(to: 4, duration: 5)
+        let demoAction2 = SKAction.rotate(byAngle: 5, duration: 5)
+//        let actionGroup = SKAction.group([demoAction1, demoAction2])
+        let actionSequence = SKAction.sequence([demoAction1, demoAction2])
+        mySprite.run(actionSequence)
+//        mySprite.run(actionGroup)
+//        mySprite.run(demoAction)
     }
 }
--- PierreThePenguinDos/GameViewController.swift
+++ PierreThePenguinDos/GameViewController.swift
@@ -12,36 +12,31 @@ import GameplayKit
 
 class GameViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
         
-        if let view = self.view as! SKView? {
-            // Load the SKScene from 'GameScene.sks'
+        if let view = self.view as? SKView {
+            // Load the SKScene form 'GameScene.sks'
             if let scene = SKScene(fileNamed: "GameScene") {
-                // Set the scale mode to scale to fit the window
+                // Set the scale mode fit the window:
                 scene.scaleMode = .aspectFill
-                
-                // Present the scene
+                // Size our scene to fit the view exactly:
+                scene.size = view.bounds.size
+                // Show the new scene:
                 view.presentScene(scene)
             }
-            
             view.ignoresSiblingOrder = true
-            
             view.showsFPS = true
             view.showsNodeCount = true
         }
     }
-
+    
     override var shouldAutorotate: Bool {
         return true
     }
 
     override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
-        if UIDevice.current.userInterfaceIdiom == .phone {
-            return .allButUpsideDown
-        } else {
-            return .all
-        }
+        return .landscape
     }
 
     override func didReceiveMemoryWarning() {
