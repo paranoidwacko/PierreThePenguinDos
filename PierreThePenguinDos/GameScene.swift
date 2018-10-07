beb61813d79cdf75077632b63c4407dd76900297
Sparrow <admin@sparrowhawks-Mac-mini.local>
2018-10-07 15:28:32 -0500

    Game now shows a ground, two hovering bees and a hovering pierre the penguin

--- PierreThePenguinDos/GameScene.swift
+++ PierreThePenguinDos/GameScene.swift
@@ -10,57 +10,33 @@ import SpriteKit
 
 class GameScene: SKScene {
     let cam = SKCameraNode()
-    let bee = SKSpriteNode()
+    let ground = Ground()
+    let player = Player()
     
     override func didMove(to view: SKView) {
         self.anchorPoint = .zero
         self.backgroundColor = UIColor(red: 0.4, green: 0.6, blue: 0.95, alpha: 1.0)
-        
         self.camera = self.cam
-        self.addTheFlyingBee()
+        
+        let bee2 = Bee()
+        bee2.position = CGPoint(x: 325, y: 325)
+        self.addChild(bee2)
+        let bee3 = Bee()
+        bee3.position = CGPoint(x: 200, y: 325)
+        self.addChild(bee3)
+        
+        self.ground.position = CGPoint(x: -self.size.width * 2, y: 150)
+        self.ground.size = CGSize(width: self.size.width * 6, height: 0)
+        ground.createChildren()
+        self.addChild(self.ground)
+        
+        self.player.position = CGPoint(x: 150, y: 250)
+        self.addChild(self.player)
     }
     
     override func didSimulatePhysics() {
         if let myCam = self.camera {
-            myCam.position = self.bee.position
+            myCam.position = self.player.position
         }
-    }
-        
-    func addTheFlyingBee() {
-        self.bee.position = CGPoint(x: 250, y: 250)
-        self.bee.size = CGSize(width: 28, height: 24)
-        self.addChild(self.bee)
-        
-        let beeAtlas = SKTextureAtlas(named: "Enemies")
-        let beeFrames:[SKTexture] = [
-            beeAtlas.textureNamed("bee"),
-            beeAtlas.textureNamed("bee-fly")]
-        let flyAction = SKAction.animate(with: beeFrames, timePerFrame: 0.14)
-        let beeAction = SKAction.repeatForever(flyAction)
-        bee.run(beeAction)
-        
-        let pathLeft = SKAction.moveBy(x: -200, y: -10, duration: 2)
-        let pathRight = SKAction.moveBy(x: 200, y: 10, duration: 2)
-        let flipTextureNegative = SKAction.scaleX(to: -1, duration: 0)
-        let flipTexturePositive = SKAction.scaleX(to: 1, duration: 0)
-        let flightOfTheBee = SKAction.sequence([pathLeft, flipTextureNegative, pathRight, flipTexturePositive])
-        let neverEndingFlight = SKAction.repeatForever(flightOfTheBee)
-        bee.run(neverEndingFlight)
-        
-//        self.anchorPoint = .zero
-//        let mySprite = SKSpriteNode(color: .blue, size: CGSize(width: 50, height: 50))
-//        mySprite.position = CGPoint(x: 150, y: 150)
-////        mySprite.anchorPoint = CGPoint(x: 0, y: 0)
-//        self.addChild(mySprite)
-//
-////        let demoAction = SKAction.move(to: CGPoint(x: 300, y: 150), duration: 3)
-////        let demoAction = SKAction.scale(to: 4, duration: 5)
-//        let demoAction1 = SKAction.scale(to: 4, duration: 5)
-//        let demoAction2 = SKAction.rotate(byAngle: 5, duration: 5)
-////        let actionGroup = SKAction.group([demoAction1, demoAction2])
-//        let actionSequence = SKAction.sequence([demoAction1, demoAction2])
-//        mySprite.run(actionSequence)
-////        mySprite.run(actionGroup)
-////        mySprite.run(demoAction)
     }
 }
